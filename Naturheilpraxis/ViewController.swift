d4b82b3b5a2e2fe36e080bc28b366feee4307554
Falko Schumann <falko.schumann@muspellheim.de>
2019-03-13 21:22:33 +0100

    Xcode-Projekt angelegt

---
+++ Naturheilpraxis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Naturheilpraxis
+//
+//  Created by Falko Schumann on 13.03.19.
+//  Copyright © 2019 Falko Schumann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ NaturheilpraxisTests/NaturheilpraxisTests.swift
@@ -0,0 +1,34 @@
+//
+//  NaturheilpraxisTests.swift
+//  NaturheilpraxisTests
+//
+//  Created by Falko Schumann on 13.03.19.
+//  Copyright © 2019 Falko Schumann. All rights reserved.
+//
+
+import XCTest
+@testable import Naturheilpraxis
+
+class NaturheilpraxisTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
